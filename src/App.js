7b17c2afcf952c604b35b046c1708769d5c326fd
gustavo <gustavo>
2023-11-10 12:00:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Instituto Honorio Delgado Espinoza
+        </p>
+        <br/>
+        <p>
+          Gustavo Renzo Abarca Hilaquita
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          turno dia 10/11/2023 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
